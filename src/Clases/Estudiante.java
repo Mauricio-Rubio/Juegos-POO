31cd163bf4f2ffccca4abb929e228540139bf9e3
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-01-24 13:02:13 -0600

    Setter and getters

--- src/Clases/Estudiante.java
+++ src/Clases/Estudiante.java
@@ -4,6 +4,22 @@ public class Estudiante extends Persona {
     private String carrera;
     private String id;
 
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public Estudiante(String nombre, int edad, String sexo, String id, String carrera){
         super(nombre,sexo,edad);
 
--- src/Clases/Persona.java
+++ src/Clases/Persona.java
@@ -10,4 +10,27 @@ public class Persona {
         this.edad= edad;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
