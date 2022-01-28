99871c34a3e1ecc5b04a3b5ae0c252679105d781
TheMotherFucker29 <mau.r.haro54@gmail.com>
2022-01-28 00:03:22 -0600

    Metodo desordenar arrreglo

--- src/Sistema/Interfaz.java
+++ src/Sistema/Interfaz.java
@@ -2,36 +2,51 @@ package Sistema;
 
 import Clases.Estudiante;
 
-public  class  Interfaz {
-    public  String generarID(String nombre, String dia, String year){
-        int numero = (int)(Math.random()*1000+1);
-        return String.valueOf(numero)+nombre.charAt(0)+nombre.charAt(2)+year.charAt(2)+year.charAt(3);
-    }
-    public String generarID(String nombre){
-        int numero = (int)(Math.random()*1000+1);
-        return String.valueOf(numero)+nombre.charAt(0)+nombre.charAt(1);
+public class Interfaz {
+    public String generarID(String nombre, String dia, String year) {
+        int numero = (int) (Math.random() * 1000 + 1);
+        return String.valueOf(numero) + nombre.charAt(0) + nombre.charAt(2) + year.charAt(2) + year.charAt(3);
     }
 
-    public Estudiante [] [] distribuirJugadores(Estudiante [] students){
-        Estudiante [] [] studentsRepartition;
+    public String generarID(String nombre) {
+        int numero = (int) (Math.random() * 1000 + 1);
+        return String.valueOf(numero) + nombre.charAt(0) + nombre.charAt(1);
+    }
+
+    public Estudiante[][] distribuirJugadores(Estudiante[] students) {
+        Estudiante[][] studentsRepartition;
         int modulo = students.length % 2;
-        if(modulo == 0){
+        if (modulo == 0) {
             System.out.println("Es par");
-            studentsRepartition = new Estudiante[2][students.length/2];
-            for(int i = 0; i<studentsRepartition[0].length; i++){
-                studentsRepartition[0][i] = students[2*i];
-                studentsRepartition[1][i] = students[(2*(i+1))-1];
+            studentsRepartition = new Estudiante[2][students.length / 2];
+            for (int i = 0; i < studentsRepartition[0].length; i++) {
+                studentsRepartition[0][i] = students[2 * i];
+                studentsRepartition[1][i] = students[(2 * (i + 1)) - 1];
             }
-        }else{
+        } else {
             System.out.println("Es impar");
-            studentsRepartition = new Estudiante[2][(students.length+1)/2];
-            for(int i = 0; i<studentsRepartition[0].length; i++){
-                studentsRepartition[0][i]=students[2*i];
+            studentsRepartition = new Estudiante[2][(students.length + 1) / 2];
+            for (int i = 0; i < studentsRepartition[0].length; i++) {
+                studentsRepartition[0][i] = students[2 * i];
             }
-            for(int i = 0; i<studentsRepartition[0].length-1; i++){
-                studentsRepartition[1][i] = students[(2*(i+1))-1];
+            for (int i = 0; i < studentsRepartition[0].length - 1; i++) {
+                studentsRepartition[1][i] = students[(2 * (i + 1)) - 1];
             }
-            studentsRepartition[1][(students.length-1)/2] = new Estudiante("IA",generarID("IA"));
-        }return studentsRepartition;
+            studentsRepartition[1][(students.length - 1) / 2] = new Estudiante("IA", generarID("IA"));
+        }
+        return studentsRepartition;
+    }
+
+    public Estudiante[] desordenarArreglo(Estudiante[] students) {
+        int a, b;
+        Estudiante aux;
+        a = students.length;
+        for (int i = 0; i <= a / 2; i++) {
+            aux=students[i];
+            students[i]=students[a-1];
+            students[a-1]=aux;
+            a--;
+        }
+    return  students;
     }
 }
