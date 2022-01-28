adcbb02a310d11f0b1f259112393c23f5cd0c9dc
TheMotherFucker29 <mau.r.haro54@gmail.com>
2022-01-28 14:29:23 -0600

    Simulacion exitosa de blackjack

--- src/Juegos/BlackJack/IABlackJack.java
+++ src/Juegos/BlackJack/IABlackJack.java
@@ -1,9 +1,8 @@
 package Juegos.BlackJack;
 
-import IA.IA;
-
-public class IABlackJack extends IA {
-    public IABlackJack(String nombre){
-        super(nombre,"");
+public class IABlackJack  {
+    public int random2() {
+        //int n = (int) (Math.random() * (<número_máximo + 1> - <número_mínimo>)) + <numero_mínimo>;
+        return (int) (Math.random() * (2 + 1 - 1)) + 1;
     }
 }
--- src/Juegos/BlackJack/miniBlackJack.java
+++ src/Juegos/BlackJack/miniBlackJack.java
@@ -1,4 +1,5 @@
 package Juegos.BlackJack;
+
 import Clases.*;
 import Juegos.Jugar;
 import jdk.swing.interop.SwingInterOpUtils;
@@ -6,26 +7,97 @@ import jdk.swing.interop.SwingInterOpUtils;
 public class miniBlackJack extends Jugar {
     //private Estudiante [] [] jugadores;
     private Baraja baraja;
-    public miniBlackJack(Estudiante [][] jugadores)
-    {
+    private IABlackJack IA = new IABlackJack();
+
+    public miniBlackJack(Estudiante[][] jugadores) {
         super(jugadores);
         this.jugadores = jugadores;
         //baraja=new Baraja();
     }
 
-    public Estudiante [][] jugar(){
+    public Estudiante[][] jugar() {
         int PARTIDAS = jugadores[0].length;
-        System.out.println("Partidas por simular--->"+PARTIDAS);
+        System.out.println("Partidas por simular--->" + PARTIDAS);
         //System.out.println("c1-->"+baraja.tomarCarta());
         //System.out.println("c2-->"+baraja.tomarCarta());
-        for(int i = 1; i<= PARTIDAS; i++){
-            System.out.println("Simulando partida "+i);
-            baraja=new Baraja();
+        for (int i = 1; i <= PARTIDAS; i++) {
+            System.out.println("\nSimulando partida " + i);
+            baraja = new Baraja();
             baraja.revolverBaraja();
-            System.out.print("Jugador "+jugadores[0][i-1].getId()+" tiene la carta "+baraja.tomarCarta());
-            System.out.println("Jugador "+jugadores[1][i-1].getId()+" tiene la carta "+baraja.tomarCarta());
+            Carta j1 = baraja.tomarCarta(), j11, j2 = baraja.tomarCarta(), j21;
+            System.out.println("Jugador " + jugadores[0][i - 1].getId() + " tiene la carta " + j1);
+            System.out.print("Jugador " + jugadores[1][i - 1].getId() + " tiene la carta " + j2+"\n");
+            //System.out.print(elegir(jugadores[0][i - 1]));
+            int manoJ1, manoJ2;
+            if (!elegir(jugadores[0][i - 1])) {
+                manoJ1 = j1.obtenerValor();
+            } else {
+                j11 = baraja.tomarCarta();
+                System.out.println("Jugador " + jugadores[0][i - 1].getId() + " tiene la carta " + j11);
+                manoJ1 = j1.obtenerValor() + j11.obtenerValor();
+            }
+            if (!elegir(jugadores[1][i - 1])) {
+                manoJ2 = j2.obtenerValor();
+                //System.out.print("\nJugador " + jugadores[0][i - 1].getId() + " tiene la mano: " + manoJ1);
+            } else {
+                j21 = baraja.tomarCarta();
+                System.out.println("Jugador " + jugadores[1][i - 1].getId() + " tiene la carta " + j21);
+                manoJ2 = j2.obtenerValor() + j21.obtenerValor();
+            }
+            int winCond = ganador(manoJ1, manoJ2);
+            Estudiante winner = null;
+            switch (winCond) {
+                case 0:
+                    winner = jugadores[1][i - 1];
+                    break;
+                case 1:
+                    winner = jugadores[0][i - 1];
+                    break;
+                case 2:
+
+                    winner = jugadores[0][i - 1];
+                    break;
+                case 3:
+                    winner = jugadores[1][i - 1];
+                    break;
+                case 4:
+                    System.out.println("empate");
+            }
+            try{System.out.println("Jugador " + winner.getId() + " ha ganado ");}
+            catch (Exception e){}
+
+            //System.out.println("Mano de " + jugadores[0][i - 1].getId() + " es " + manoJ1);
+
+
             //System.out.println();
         }
+        System.out.println("");
         return null;
+    }
+
+    public boolean elegir(Estudiante jugador) {
+        int aleatorio = IA.random2();
+        if (aleatorio == 2) {
+            System.out.println("Jugador " + jugador.getId() + " ha decidido quedarse");
+            return true;
+        } else if (aleatorio == 1) {
+            System.out.println("Jugador " + jugador.getId() + " ha decidido salir");
+        }
+        return false;
+    }
+
+    public int ganador(int mano1, int mano2) {
+        int n = 0;
+        if (mano1 > 21 && mano2 <= 21) {
+            return 0;
+        } else if (mano2 > 21 && mano1 <= 21) {
+            return 1;
+        } else if (mano1 > mano2) {
+            return 2;
+        } else if (mano2 > mano1) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
